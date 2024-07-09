6dfe835831902822cb2a68def775de358850bc95
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-07-09 18:07:00 +0900

    test

--- frontend/apps/web/app/page.tsx
+++ frontend/apps/web/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>
+            こんにちわ
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
